0067be1757f54d2cb9f9f58a40fb12db666fb15e
renmin <renmin129@hotmail.com>
2017-06-16 15:52:09 +0800

    Add project

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by minren on 16/06/2017.
+//  Copyright © 2017 minren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
